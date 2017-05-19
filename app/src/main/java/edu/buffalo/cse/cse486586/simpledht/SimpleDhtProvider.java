dd236a86aa9b9b9788a83242e7cceb040936754e
Shivam Gupta <shivamgupta@Meghas-MacBook-Pro.local>
2017-05-19 14:13:49 -0400

    simple dht

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtProvider.java
@@ -0,0 +1,91 @@
+package edu.buffalo.cse.cse486586.simpledht;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.TreeMap;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class SimpleDhtProvider extends ContentProvider {
+
+    static final String TAG = SimpleDhtProvider.class.getSimpleName();
+    static final int SERVER_PORT = 10000;
+    private final Uri mUri = buildUri("content", "edu.buffalo.cse.cse486586.simpledht.provider");
+    private ContentResolver mContentResolver;
+    private ContentValues mContentValues ;
+    String portStr;
+    TreeMap<String, String> map = new TreeMap<String, String>();
+
+
+    private Uri buildUri(String scheme, String authority) {
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.authority(authority);
+        uriBuilder.scheme(scheme);
+        return uriBuilder.build();
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean onCreate() {
+        // TODO Auto-generated method stub
+        TelephonyManager tel = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+
+        try{
+
+        }catch(Exception e){
+            Log.e(TAG, "Exception in onCreate-->"+e);
+        }
+
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+            String sortOrder) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    private String genHash(String input) throws NoSuchAlgorithmException {
+        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+        byte[] sha1Hash = sha1.digest(input.getBytes());
+        Formatter formatter = new Formatter();
+        for (byte b : sha1Hash) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
+    }
+}
